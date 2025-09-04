297e13fc969a804c239d73833c0688d79c42a6c2
bornsrss14 <bornsrss14@gmail.com>
2025-09-04 04:51:13 -0600

    arreglo la lógica de mi portafolio, el navbar cerrar y abrir

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,10 +1,16 @@
 import { Link } from "react-router-dom";
 import LogoScalable from "../core/LogoScalable";
-import { IconChevronDown } from "@tabler/icons-react";
+import { IconChevronDown, IconMenu2 } from "@tabler/icons-react";
 import LanguageToggle from "../core/LanguageToggle";
 import { useState } from "react";
 
 export const Navbar = () => {
+  const [openNav, setOpenNav] = useState();
+
+  const toggleOpenClose = () => {
+    setOpenNav((prev) => !prev);
+    console.log("este eleemento está", openNav);
+  };
   const languajes = [
     {
       idiom: "EN",
@@ -21,58 +27,66 @@ export const Navbar = () => {
   };
   const viceversa = languaje.idiom === "EN" ? languajes[1] : languajes[0];
   return (
-    <div className="flex-row div-navbar">
-      <Link to={"/"}>
-        <div className="flex-row">
-          <h3>
-            <span style={{ color: "rgb(60, 60, 191)" }}>panda</span>
-            sneezing
-          </h3>
-          <LogoScalable
-            imagenImg={
-              "https://firebasestorage.googleapis.com/v0/b/bornsrss-8ab5d.appspot.com/o/portfolio%2Fpandas-black-white.png?alt=media&token=39f89382-5898-47d9-83ce-72b11a6872ca"
-            }
-            customHeight="30px"
-            customWidth="30px"
-          ></LogoScalable>
-        </div>
-      </Link>
-
-      <ul className="flex-row-navbar">
-        <li>
-          <a href="#myResumen">Resume</a>
-        </li>
-        <li>
-          <Link to={"/about-me"}>About me</Link>
-        </li>
-
-        <li>
-          <Link to={"/blog"}>Articles</Link>
-        </li>
-        <li>
-          <Link to={"/contact"}>Contact</Link>
-        </li>
-      </ul>
-      <div className="container-col">
-        <div className="flex-row-nav">
-          <p>{viceversa.idiom}</p>
-          <LogoScalable
-            imagenImg={viceversa.flag}
-            customHeight={"1.3rem"}
-            customWidth={"1.3rem"}
-          />
-          <IconChevronDown />
-
-          <div className="submenu">
-            <LanguageToggle
-              toggleLanguaje={toggleLanguaje}
-              languaje={languaje}
-              setLanguaje={setLanguaje}
-            />
+    <>
+      <div className="div-navbar">
+        <Link to={"/"}>
+          <div className="flex-row">
+            <h3>
+              <span style={{ color: "rgb(60, 60, 191)" }}>panda</span>
+              sneezing
+            </h3>
+            <LogoScalable
+              imagenImg={
+                "https://firebasestorage.googleapis.com/v0/b/bornsrss-8ab5d.appspot.com/o/portfolio%2Fpandas-black-white.png?alt=media&token=39f89382-5898-47d9-83ce-72b11a6872ca"
+              }
+              customHeight="30px"
+              customWidth="30px"
+            ></LogoScalable>
           </div>
+        </Link>
+        <div className={openNav ? "nav-hidden-false" : "nav-hidden"}>
+          <ul className="flex-row-navbar">
+            <li>
+              <a href="#myResumen">Resume</a>
+            </li>
+            <li>
+              <Link to={"/about-me"}>About me</Link>
+            </li>
+
+            <li>
+              <Link to={"/blog"}>Articles</Link>
+            </li>
+            <li>
+              <Link to={"/contact"}>Contact</Link>
+            </li>
+          </ul>
+          <div className="container-col">
+            <div className="flex-row-nav">
+              <p>{viceversa.idiom}</p>
+              <LogoScalable
+                imagenImg={viceversa.flag}
+                customHeight={"1.3rem"}
+                customWidth={"1.3rem"}
+              />
+              <IconChevronDown />
+
+              <div className="submenu">
+                <LanguageToggle
+                  toggleLanguaje={toggleLanguaje}
+                  languaje={languaje}
+                  setLanguaje={setLanguaje}
+                />
+              </div>
+            </div>
+          </div>
+
+          <p> contenedor de lista</p>
         </div>
+        <button className="btn-navbar" onClick={toggleOpenClose}>
+          <IconMenu2 stroke={3} color="black" size={"22px"}></IconMenu2>
+        </button>
       </div>
-    </div>
+    </>
   );
 };
 
