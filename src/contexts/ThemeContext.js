8b6c210db88cc71c2892c80d16370ed6474a6770
bornsrss14 <bornsrss14@gmail.com>
2025-11-09 00:44:46 -0600

    Artículo de uso de context

--- src/contexts/ThemeContext.js
+++ src/contexts/ThemeContext.js
@@ -1,37 +1,36 @@
 import { createContext, useContext, useState, useMemo, useEffect } from "react";
 
 export const ThemeContext = createContext();
-
+const dark = {
+  txt: "var(--primary-white)",
+  txt_logo: "white",
+  ui: "var(--raisin-black-light)",
+  uiEduExp: "var(--night-bg)",
+  bg: "var(--night-bg)",
+  bgExperience: "var(--raisin-black-light)",
+  bgfooter: "var(--night-bg)",
+  colorBorder: "rgba(113, 113, 113, 1)",
+  tags: "var(--night-bg)",
+  folderFront: "var(--color-dark-folder-front)",
+  folderBack: "var(--color-dark-folder-back)",
+  bgMansory: "var(--raisin-black-light)",
+};
+const light = {
+  txt: "var(--night-bg)",
+  txt_logo: "var(--c-primary-blue)",
+  ui: "var(--white-dark)",
+  uiEduExp: "var(--white-dark)",
+  bg: "var(--primary-white)",
+  bgExperience: "var(--primary-white)",
+  bgfooter: "var(--white-dark)",
+  colorBorder: "rgb(5, 5, 79)",
+  tags: "rgb(0, 0, 255)",
+  folderFront: "var(--azul-pastel)",
+  folderBack: "var(--azul-pastel-dark)",
+  bgMansory: "var(--primary-white)",
+};
 export const ThemeContextProvider = ({ children }) => {
   const [isDark, setIsDark] = useState(false);
-  const dark = {
-    txt: "var(--primary-white)",
-    txt_logo: "white",
-    ui: "var(--raisin-black-light)",
-    uiEduExp: "var(--night-bg)",
-    bg: "var(--night-bg)",
-    bgExperience: "var(--raisin-black-light)",
-    bgfooter: "var(--night-bg)",
-    colorBorder: "rgba(113, 113, 113, 1)",
-    tags: "var(--night-bg)",
-    folderFront: "var(--color-dark-folder-front)",
-    folderBack: "var(--color-dark-folder-back)",
-    bgMansory: "var(--raisin-black-light)",
-  };
-  const light = {
-    txt: "var(--night-bg)",
-    txt_logo: "var(--c-primary-blue)",
-    ui: "var(--white-dark)",
-    uiEduExp: "var(--white-dark)",
-    bg: "var(--primary-white)",
-    bgExperience: "var(--primary-white)",
-    bgfooter: "var(--white-dark)",
-    colorBorder: "rgb(5, 5, 79)",
-    tags: "rgb(0, 0, 255)",
-    folderFront: "var(--azul-pastel)",
-    folderBack: "var(--azul-pastel-dark)",
-    bgMansory: "var(--primary-white)",
-  };
 
   const toggleTheme = () => setIsDark((prev) => !prev);
   // 🧩 aplica clases al body
